8bbae98ce0efb7b9bef8ef0721283c98aec9e04b
desertsniper87 <torshobuet@gmail.com>
2024-12-03 10:45:07 +0600

    [master] [03Dec-1045] ok

--- day-1/main.go
+++ day-1/main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,21 +18,44 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
-	}
+	set1 := make([]int, 1000)
+	set2 := make([]int, 1000)
 
+	i := 0
 	for scanner.Scan() {
-		num, err := strconv.Atoi(scanner.Text())
+		s := strings.Fields(scanner.Text())
+		set1[i], err = strconv.Atoi(s[0])
 		if err != nil {
-			fmt.Printf("Error converting to number: %v\n", err)
+			fmt.Printf("Error converting string to int: %v\n", err)
 			return
 		}
-		fmt.Println(num)
+		set2[i], err = strconv.Atoi(s[1])
+		if err != nil {
+			fmt.Printf("Error converting string to int: %v\n", err)
+			return
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error reading file: %v\n", err)
+		}
+
+		i++
 	}
 
+	sort.Ints(set1)
+	sort.Ints(set2)
+
+	sum := 0
+	for i, v1 := range set1 {
+		v2 := set2[i]
+
+		if v1 > v2 {
+			sum += v1 - v2
+		} else {
+			sum += v2 - v1
+		}
+	}
+
+	fmt.Print(sum)
 }
